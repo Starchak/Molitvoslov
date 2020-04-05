6b5f96e671bb069deaab43ad2018d730670248a0
Vlad Stolyarenko <vladislav.stolyarenko@gmail.com>
2020-04-06 00:53:25 +0300

    [WIP]continue work on player

---
+++ src/components/ProgressBar/styles.ts
@@ -0,0 +1,11 @@
+import {Dimensions, StyleSheet} from 'react-native';
+
+const {width: SCREEN_WIDTH} = Dimensions.get('window');
+
+export default StyleSheet.create({
+  play_bg: {
+    height: '70%',
+    position: 'absolute',
+    backgroundColor: "#000000"
+  },
+});
