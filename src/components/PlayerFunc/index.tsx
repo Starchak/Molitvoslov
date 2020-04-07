d74b0a516c655bedbef02aaa2a36aefeb908ddde
Maksym Maksymenko <imaksmaksimenko@gmail.com>
2020-04-07 16:52:46 -0400

    Play/Pause/Download

--- src/components/PlayerFunc/index.tsx
+++ src/components/PlayerFunc/index.tsx
@@ -3,10 +3,12 @@ import TrackPlayer, {
   Track,
   useProgress,
 } from 'react-native-track-player';
-import {Image, TouchableHighlight, View} from 'react-native';
+import {Image, TouchableOpacity, View} from 'react-native';
 import styles from '../Player/styles';
-import pause from '../../assets/img/pause.png';
-import React, {useEffect} from 'react';
+import play_img from '../../assets/img/play_active.png';
+import download_img from '../../assets/img/play.png';
+import pause_img from '../../assets/img/pause.png';
+import React, {useEffect, useState} from 'react';
 import {ProgressBar} from '../ProgressBar';
 
 type Props = {
@@ -14,7 +16,13 @@ type Props = {
   test: any;
 };
 
+let playInterval: any;
+
 export default function Player(props: Props) {
+  const [isDownload, setIsDownload] = useState(true);
+  const [isPlay, setIsPlay] = useState(false);
+  const [progress, setProgress] = useState({position: 0, duration: 0});
+
   useEffect(() => {
     console.log('mount');
   });
@@ -22,56 +30,92 @@ export default function Player(props: Props) {
   TrackPlayer.destroy();
 
   const Progress = () => {
-    let progress = useProgress();
-    return <ProgressBar SeekTo={SeekTo} progress={progress} />;
+    let progress: any;
+
+    TrackPlayer.getPosition().then((position) => {
+      console.log(position);
+      console.log(props);
+      progress = {position: position, duration: props.track.duration};
+      setProgress(progress);
+      if (position >= progress.duration - 0.8) {
+        Pause(true);
+      }
+    });
   };
 
   async function SeekTo(pos: number) {
     TrackPlayer.getPosition().then((position) => {
       console.log(position);
-      TrackPlayer.seekTo(10).then((p) => {
+      TrackPlayer.seekTo(pos).then((p) => {
         console.log(p);
       });
     });
   }
 
   async function PlayPause() {
-    TrackPlayer.destroy();
-    TrackPlayer.setupPlayer().then(() => {
-      TrackPlayer.updateOptions({
-        capabilities: [
-          Capability.Play,
-          Capability.Pause,
-          Capability.Stop,
-          Capability.SeekTo,
-        ],
-        compactCapabilities: [
-          Capability.Play,
-          Capability.Pause,
-          Capability.Stop,
-          Capability.SeekTo,
-        ],
-        notificationCapabilities: [
-          Capability.Play,
-          Capability.Pause,
-          Capability.Stop,
-          Capability.SeekTo,
-        ],
-        icon: require('../../assets/img/pray.png'),
-      });
-    });
+    if (isDownload) {
+      // Vlad write code for download here
+    }
 
-    TrackPlayer.add(props.track).then(() => {
-      TrackPlayer.play();
-    });
+    if (isPlay) {
+      Pause(false);
+    } else {
+      TrackPlayer.setupPlayer().then(() => {
+        TrackPlayer.updateOptions({
+          capabilities: [
+            Capability.Play,
+            Capability.Pause,
+            Capability.Stop,
+            Capability.SeekTo,
+          ],
+          compactCapabilities: [
+            Capability.Play,
+            Capability.Pause,
+            Capability.Stop,
+            Capability.SeekTo,
+          ],
+          notificationCapabilities: [
+            Capability.Play,
+            Capability.Pause,
+            Capability.Stop,
+            Capability.SeekTo,
+          ],
+          icon: require('../../assets/img/pray.png'),
+        });
+      });
+
+      TrackPlayer.add(props.track).then(() => {
+        TrackPlayer.play();
+      });
+
+      playInterval = setInterval(Progress, 1000);
+
+      setIsPlay(true);
+    }
   }
+
+  const Pause = (isStop: boolean) => {
+    if (isStop) {
+      TrackPlayer.stop();
+    } else {
+      TrackPlayer.pause();
+    }
+    setIsPlay(false);
+    clearInterval(playInterval);
+  };
 
   return (
     <View style={styles.player}>
-      <TouchableHighlight style={styles.play} onPress={() => PlayPause()}>
-        <Image style={styles.play_img} source={pause} />
-      </TouchableHighlight>
-      <Progress SeekTo={SeekTo} />
+      <TouchableOpacity style={styles.play} onPress={() => PlayPause()}>
+        {isDownload ? (
+          <Image style={styles.play_img} source={download_img} />
+        ) : isPlay ? (
+          <Image style={styles.play_img} source={pause_img} />
+        ) : (
+          <Image style={styles.play_img} source={play_img} />
+        )}
+      </TouchableOpacity>
+      <ProgressBar SeekTo={SeekTo} progress={progress} />
     </View>
   );
 }
